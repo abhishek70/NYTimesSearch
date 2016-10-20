e041665551643124a418b00dad9fade25ee52c07
Abhishek Desai <abhishek.desai70@gmail.com>
2016-10-19 20:39:37 -0700

    Added utils packkage for app config and constants

---
+++ app/src/main/java/com/example/abhishek/nytimessearch/utils/ConfigExample.java
@@ -0,0 +1,16 @@
+package com.example.abhishek.nytimessearch.utils;
+
+/**
+ * Created by abhishek on 10/16/16.
+ * Rename the file with Config.java and the class name to Config
+ * Add actual NYTimes Article API Key
+ */
+
+public final class ConfigExample {
+
+    /** NYTimes Article API KEY */
+    public static final String NYTIMES_ARTICLE_API_KEY = "YOUR_API_KEY";
+
+    /** NYTimes Article API URL */
+    public static final String NYTIMES_ARTICLE_API_URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
+}
---
+++ app/src/main/java/com/example/abhishek/nytimessearch/utils/Constant.java
@@ -0,0 +1,10 @@
+package com.example.abhishek.nytimessearch.utils;
+
+/**
+ * Created by abhishek on 10/14/16.
+ */
+
+public final class Constant {
+
+
+}
